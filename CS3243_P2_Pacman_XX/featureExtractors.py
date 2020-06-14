854833704ec4cc40985530a777c3c64c723ac5eb
Nauman-S <58757898+Nauman-S@users.noreply.github.com>
2020-06-14 22:46:47 +0800

    Update featureExtractors.py
    "#-of-ghosts-1-step-away"  to "#-of-active-ghosts-1-step-away"

--- CS3243_P2_Pacman_XX/featureExtractors.py
+++ CS3243_P2_Pacman_XX/featureExtractors.py
@@ -147,7 +147,7 @@ class NewExtractor(FeatureExtractor):
             ((next_x, next_y) in Actions.getLegalNeighbors(g, walls))
             for g in scared_ghosts_positions)
         # if there is no danger of ghosts then add the food feature
-        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
+        if not features["#-of-active-ghosts-1-step-away"] and food[next_x][next_y]:
             features["eats-food"] = 1.0
 
         dist = closestFood((next_x, next_y), food, walls)
